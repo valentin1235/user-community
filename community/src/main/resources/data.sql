c2335b83a8d942b28a72b25b703056179d676769
hee c <valentin1679918@gmail.com>
2022-05-27 13:11:47 +0900

    wip

--- community/src/main/resources/data.sql
+++ community/src/main/resources/data.sql
@@ -12,10 +12,10 @@ INSERT INTO postings (id, title, content, created_at, updated_at, users_id) VALU
 INSERT INTO postings (id, title, content, created_at, updated_at, users_id) VALUES (6, "title6", "content6", "2022-05-25T03:03:12", "2022-05-25T03:03:12", 1);
 INSERT INTO postings (id, title, content, created_at, updated_at, users_id) VALUES (7, "title7", "content7", "2022-05-26T03:03:12", "2022-05-26T03:03:12", 1);
 
-INSERT INTO likes (id, postings_id, users_id, updated_at, created_at) VALUES (1, 1, 1, "2022-05-20T03:03:12", "2022-05-20T03:03:12");
-INSERT INTO likes (id, postings_id, users_id, updated_at, created_at) VALUES (2, 1, 2, "2022-05-21T03:03:12", "2022-05-21T03:03:12");
-INSERT INTO likes (id, postings_id, users_id, updated_at, created_at) VALUES (3, 1, 3, "2022-05-22T03:03:12", "2022-05-22T03:03:12");
-INSERT INTO likes (id, postings_id, users_id, updated_at, created_at) VALUES (4, 1, 4, "2022-05-23T03:03:12", "2022-05-23T03:03:12");
+INSERT INTO likes (id, postings_id, users_id, updated_at, created_at) VALUES (1, 7, 1, "2022-05-20T03:03:12", "2022-05-20T03:03:12");
+INSERT INTO likes (id, postings_id, users_id, updated_at, created_at) VALUES (2, 7, 2, "2022-05-21T03:03:12", "2022-05-21T03:03:12");
+INSERT INTO likes (id, postings_id, users_id, updated_at, created_at) VALUES (3, 7, 3, "2022-05-22T03:03:12", "2022-05-22T03:03:12");
+INSERT INTO likes (id, postings_id, users_id, updated_at, created_at) VALUES (4, 7, 4, "2022-05-23T03:03:12", "2022-05-23T03:03:12");
 
 INSERT INTO comments (id, postings_id, users_id, content, updated_at, created_at) VALUES (1, 1, 1, "content1", "2022-05-21T03:03:12", "2022-05-21T03:03:12");
 INSERT INTO comments (id, postings_id, users_id, content, updated_at, created_at) VALUES (2, 1, 2, "content2", "2022-05-22T03:03:12", "2022-05-22T03:03:12");
